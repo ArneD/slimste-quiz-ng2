2e71f24d509632e30b610de705372d974ef29205
Arne Dumarey <dumarey.arne@gmail.com>
2017-01-07 15:35:52 +0100

    Added input fields to change the names to start the quiz

--- src/ui/src/app/admin/set-up/set-up.component.ts
+++ src/ui/src/app/admin/set-up/set-up.component.ts
@@ -1,3 +1,5 @@
+import { IState } from './../../core/states';
+import { Store } from '@ngrx/store';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./set-up.component.scss']
 })
 export class SetUpComponent implements OnInit {
+  player1Name: string = 'Player 1';
+  player2Name: string = 'Player 2';
+  player3Name: string = 'Player 3';
+  numberOfSeconds: number = 60;
 
-  constructor() { }
+  constructor(private store: Store<IState>) { }
 
   ngOnInit() {
   }
 
+  public submitQuiz() {
+    console.log(this.player1Name);
+    console.log('submitted');
+  }
 }
