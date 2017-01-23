58b4610e37dd52dc5369861874544a83f8687db8
Arne Dumarey <dumarey.arne@gmail.com>
2017-01-23 20:07:08 +0100

    Added onDestroy for navigation subscription

--- src/ui/src/app/client/client.component.ts
+++ src/ui/src/app/client/client.component.ts
@@ -1,6 +1,6 @@
 import { Router } from '@angular/router';
 import { StoreService } from './../core/store.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { NavigationType } from './../core/models';
 
 @Component({
@@ -8,13 +8,13 @@ import { NavigationType } from './../core/models';
   templateUrl: './client.component.html',
   styleUrls: ['./client.component.scss']
 })
-export class ClientComponent implements OnInit {
-
+export class ClientComponent implements OnInit, OnDestroy {
+  navigationSubscription;
   constructor(private storeService: StoreService, private router: Router) { }
 
   ngOnInit() {
     if (this.storeService.store) {
-      this.storeService.store
+      this.navigationSubscription = this.storeService.store
         .select(state => state.navigationState)
         .subscribe((navigation) => {
           switch (navigation.navigationType) {
@@ -29,6 +29,12 @@ export class ClientComponent implements OnInit {
               break;
           }
         });
+    }
+  }
+
+  ngOnDestroy() {
+    if (this.navigationSubscription) {
+      this.navigationSubscription.unsubscribe();
     }
   }
 }
