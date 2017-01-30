9893b908417f494e59278269f1a165c05bbd596b
Arne Dumarey <dumarey.arne@gmail.com>
2017-01-30 20:48:39 +0100

    Clean up 3-6-9 shared component

--- src/ui/src/app/shared/three-six-nine/three-six-nine.component.ts
+++ src/ui/src/app/shared/three-six-nine/three-six-nine.component.ts
@@ -6,21 +6,13 @@ import { StoreService } from './../../core/store.service';
   templateUrl: './three-six-nine.component.html',
   styleUrls: ['./three-six-nine.component.scss']
 })
-export class ThreeSixNineComponent implements OnInit, OnDestroy {
-  selectedNr; //= this.storeService.store.select(state => state.quizState.threeSixNine.numberOfQuestion);
+export class ThreeSixNineComponent implements OnInit {
+  selectedNr$ = this.storeService.store.select(state => state.quizState.threeSixNine.numberOfQuestion);
   name = new Date().getMilliseconds();
 
   constructor(private storeService: StoreService) { }
 
   ngOnInit() {
-    console.log(this.storeService);
-    this.storeService.store.select(state => state.quizState.threeSixNine).subscribe(three => {
-      this.selectedNr = three.numberOfQuestion;
-      console.log(this.name + this.selectedNr);
-    });
-  }
-
-  ngOnDestroy() {
 
   }
 }
